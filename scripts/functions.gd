653b42e76da885e6d5b6630cfe9b2d2fe27e0a53
MareCochlea <marecochlea@gmail.com>
2023-09-15 15:33:38 +0200

    new

--- scripts/functions.gd
+++ scripts/functions.gd
@@ -1,14 +1,16 @@
 extends RigidBody3D
 
+var time = Time.get_time_dict_from_system()
+
 # Debug
 
 func _ready():
 	%debug.start()
+	print("%02d:%02d:%02d" % [time.hour, time.minute, time.second])
 
 func _on_debug_timeout():
-	print(get_mass())
 	while sleeping == true:
-		print("yes")
+		print("sleeping")
 
 
 # Pyshics
@@ -19,13 +21,14 @@ func _on_mass_timeout():
 	%mass.stop()
 
 func _on_timer_timeout():
-	sleeping = true
+	pass
 
 func _on_flip_pressed():
 	set_constant_torque(Vector3(0, 10, 0))
 	set_linear_velocity(Vector3(0, 12.5, 0))
 	set_angular_velocity(Vector3(12.5, 2.5, 12.5))
 	%mass.start()
+	print("started")
 
 func _on_area_3d_body_entered(body):
 	if body_entered:
